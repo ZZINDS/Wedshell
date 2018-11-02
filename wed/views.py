42d7da59603ce5d5f7516b8d2870ede68c62b78a
cz <zzinds@outlook.com>
2018-11-02 15:11:06 +0800

    Initial commit

---
+++ wed/views.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from dwebsocket.decorators import accept_websocket, require_websocket
+from django.http import HttpResponse
+import paramiko
+
+
+
+def exec_command(comm):
+    hostname = "xxx"    #ip
+    username = "xxx"   #用户名
+    password = "xxx"   #密码
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=hostname, username=username, password=password)
+    stdin, stdout, stderr = ssh.exec_command(comm)
+    result = stdout.read()
+    ssh.close()
+    return result
+
+
+@accept_websocket
+def index(request):
+
+    if not request.is_websocket():  # 判断是不是websocket连接
+        try:  # 如果是普通的http方法
+            message = request.GET['message']
+            return HttpResponse(message)
+        except:
+            return render(request, 'index.html')
+    else:
+        for message in request.websocket:
+            # 通过页面的message传递参数
+            cmd = message
+            command = cmd #这里是要执行的命令或者脚本
+            request.websocket.send(exec_command(command))  # 发送消息到客户端
+
